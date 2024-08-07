cbbd70b2eeba9363243e829af37bb2374017ce07
GhostSomnath <somnath.21.kaushik@gmail.com>
2024-08-07 12:33:09 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,8 @@
+// vite.config.js
 export default {
   base: '/3d-portfolio/',
+  server: {
+    port: 5173, // Specify the port you want to use
+    host: '0.0.0.0', // Bind to all network interfaces
+  },
 };
